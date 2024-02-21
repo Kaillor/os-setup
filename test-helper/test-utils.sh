738223edeb2d32809c85ce7e0e7547fb31ef0800
Lukas Giger <83269035+Kaillor@users.noreply.github.com>
2024-02-21 09:09:49 +0100

    wip

--- test-helper/test-utils.sh
+++ test-helper/test-utils.sh
@@ -5,12 +5,7 @@ load "$rootDirectory/test-helper/bats/bats-support/load.bash"
 load "$rootDirectory/test-helper/bats/bats-assert/load.bash"
 
 loadScriptUnderTest() {
-    callingTestScript=$(__callingTestScript)
-    load "$(__scriptUnderTest "$callingTestScript")"
-}
-
-__callingTestScript() {
-    echo $(realpath "$(echo "$(caller 1)" | cut -d ' ' -f 2)")
+    load "$(__scriptUnderTest "${BASH_SOURCE[1]}")"
 }
 
 __scriptUnderTest() {

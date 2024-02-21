e682ca50b40321159b69fc68c7fed7170d5ef18e
Lukas Giger <83269035+Kaillor@users.noreply.github.com>
2024-02-21 07:47:08 +0100

    wip

--- test-helper/test-utils.sh
+++ test-helper/test-utils.sh
@@ -1,1 +1,5 @@
 #!/bin/bash
+rootDirectory=$(realpath "$(dirname "${BASH_SOURCE[0]}")/..")
+
+load "$rootDirectory/test-helper/bats/bats-support/load.bash"
+load "$rootDirectory/test-helper/bats/bats-assert/load.bash"

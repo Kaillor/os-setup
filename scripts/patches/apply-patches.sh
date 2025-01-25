ffc1c36b15d10f49c277fc29ccdae58bdbb60764
Lukas Giger <83269035+Kaillor@users.noreply.github.com>
2025-01-25 19:45:15 +0100

    add shellformat

--- scripts/patches/apply-patches.sh
+++ scripts/patches/apply-patches.sh
@@ -2,19 +2,19 @@
 source "./patch-utils.sh"
 
 usage() {
-    echo "Usage: $(basename "$0") <file>"
-    echo "  file        File that contains a list of absolute filepaths which"
-    echo "              need to be patched. Corresponding patch files need to"
-    echo "              be placed next to this file in a directory called"
-    echo "              $patchDirectoryName and have the same name as the file"
-    echo "              plus the extension '.patch'. Original files will be"
-    echo "              backed up in a directory called $backupDirectoryName"
-    echo "              next to this file."
-    exit 1
+  echo "Usage: $(basename "$0") <file>"
+  echo "  file        File that contains a list of absolute filepaths which"
+  echo "              need to be patched. Corresponding patch files need to"
+  echo "              be placed next to this file in a directory called"
+  echo "              $patchDirectoryName and have the same name as the file"
+  echo "              plus the extension '.patch'. Original files will be"
+  echo "              backed up in a directory called $backupDirectoryName"
+  echo "              next to this file."
+  exit 1
 }
 
-if [[ ! "$#" -eq 1 ]]; then
-    usage
+if [[ ! $# -eq 1 ]]; then
+  usage
 fi
 
 fileWithListOfFilesToPatch="$(realpath "$1")"
--- scripts/patches/revert-patches.sh
+++ scripts/patches/revert-patches.sh
@@ -2,17 +2,17 @@
 source "./patch-utils.sh"
 
 usage() {
-    echo "Usage: $(basename "$0") <file>"
-    echo "  file        File that contains a list of absolute filepaths which"
-    echo "              need to be reverted. Corresponding original files need"
-    echo "              to be placed next to this file in a directory called"
-    echo "              $backupDirectoryName and have the same name as the"
-    echo "              file."
-    exit 1
+  echo "Usage: $(basename "$0") <file>"
+  echo "  file        File that contains a list of absolute filepaths which"
+  echo "              need to be reverted. Corresponding original files need"
+  echo "              to be placed next to this file in a directory called"
+  echo "              $backupDirectoryName and have the same name as the"
+  echo "              file."
+  exit 1
 }
 
-if [[ ! "$#" -eq 1 ]]; then
-    usage
+if [[ ! $# -eq 1 ]]; then
+  usage
 fi
 
 fileWithListOfFilesToRevert="$(realpath "$1")"

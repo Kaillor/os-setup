9d326c970de1d121ab6c715c6281b7edb074d63b
Lukas Giger <83269035+Kaillor@users.noreply.github.com>
2025-04-20 19:22:03 +0200

    improve usage commands of executable scripts

--- script/patch/apply-patches.sh
+++ script/patch/apply-patches.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 usage() {
-  printf "Usage: %s <path>\n" "$(basename "${BASH_SOURCE[0]}")"
-  printf "  path        Path that corresponds to a file containing a list of\n"
-  printf "              absolute file paths that need to be patched, or a\n"
-  printf "              directory. If the path is a directory, all child\n"
-  printf "              directories are searched for files named\n"
-  printf "              '%s' which must contain a list of absolute\n" "$FILES_TO_PATCH_NAME"
-  printf "              file paths that need to be patched. Corresponding patch\n"
-  printf "              files must be placed next to each file processed this\n"
-  printf "              way in a directory called '%s' and have the same\n" "$PATCH_DIRECTORY_NAME"
-  printf "              name as the file to be patched plus the extension\n"
-  printf "              '.patch'. The original files will be backed up in the\n"
-  printf "              original directory with the extension '.orig'.\n"
+  cat << EOF
+Usage: $(basename "${BASH_SOURCE[0]}") <path>
+  path        Path that corresponds to a file containing a list of absolute
+              file paths that need to be patched, or a directory. If the path
+              is a directory, all child directories are searched for files
+              named '$FILES_TO_PATCH_NAME' which must contain a list of absolute file
+              paths that need to be patched. Corresponding patch files must be
+              placed next to each file processed this way in a directory called
+              '$PATCH_DIRECTORY_NAME' and have the same name as the file to be patched plus
+              the extension '.patch'. The original files will be backed up in
+              the original directory with the extension '.orig'.
+EOF
 }
 
 main() {

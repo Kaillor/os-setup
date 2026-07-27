b3aa8838b6a607bee27e314a36a65cfd95ab92cf
Lukas Giger <83269035+Kaillor@users.noreply.github.com>
2026-07-27 15:36:58 +0200

    Revert "remove cinnamon grouped-window-list patch (#129)"
    This reverts commit 519607d7d0fdc87785e55ea0be6786fa97dffcb6.
    
    The grouped-window-list setting does not work as expected.

--- setup/debian/mint/cinnamon/install/common/install.sh
+++ setup/debian/mint/cinnamon/install/common/install.sh
@@ -1,1 +1,12 @@
 #!/bin/bash
+main() {
+  local script_directory
+  script_directory="$(dirname "${BASH_SOURCE[0]}")"
+  source "$script_directory/../../../../../../script/script-util.sh"
+
+  apply_patches "$script_directory/patches"
+
+  return 0
+}
+
+main "$@"
